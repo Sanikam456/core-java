7b7d1ce74d29e7fd18d6b02db71323a88fa6810b
Sanikam456 <sanikamsanikam7@gmail.com>
2026-04-05 14:32:23 +0530

    test

---
+++ BankAccountRunner.java
@@ -0,0 +1,8 @@
+class BankAccountRunner{
+  public static void main(String []bank){
+BankAccount.getBankAccountDetails();
+BankAccount.getBankAccountDetails1(40.000);
+BankAccount.getBankAccountDetails2();
+BankAccount.getBankAccountDetails3("Karnataka");
+}
+}
